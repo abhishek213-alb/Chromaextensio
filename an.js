3e0d757651338ec3edd271ba28ed9f7dae48524a
Abhishek <114422422+starkdaddy27@users.noreply.github.com>
2022-10-12 13:17:49 +0530

    Create an.js

---
+++ an.js
@@ -0,0 +1,7 @@
+const API_URL = "https://api.jikan.moe/v3/search/anime"
+
+const resForm = document.querySelector(".result-form")
+const resSearchText = document.querySelector(".result-form__input")
+
+const resImage = document.querySelector(".result__img")
+const resTitle = document.querySelector(".result__title")
